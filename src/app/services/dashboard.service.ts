011ef8f2dd2bb1dc936233fcecd9463cf75fbc81
satish <satishkonduru0225@gmail.com>
2024-09-29 12:36:34 +0530

    Add New Book -Fixing Kown Issue

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,19 @@ export class DashboardService {
   http = inject(HttpClient)
  
   addNewBook(userId: string, bookName: string){
-  return  this.http.patch(`${this.url}/users/${userId}`, {books: {title: bookName}})
-  }
+    // return  this.http.patch(`${this.url}/users/${userId}`, {books: {title: bookName}})  
+    return this.http.get(`${this.url}/users/${userId}`).pipe(
+      switchMap((user: any) => {
+        const updatedBooks = user.books ? [...user.books, {bookTitle: bookName}] : [{bookTitle: bookName}]
+        return this.http.patch(`${this.url}/users/${userId}`, {books: updatedBooks})
+      })
+    )
 
+  }
+/*
+  1. When the GET request completes and emits the user data, 'switchMap() recevies this data and uses it to create a new Obsevable with PATCH
+  2. the PATCH request updates the user's books list with the new book
+  3. If new call to PATCH, switchMAp will cancel the previous PATCH Req. ensuring that only the latest update is procesed.
+*/
 
 }
