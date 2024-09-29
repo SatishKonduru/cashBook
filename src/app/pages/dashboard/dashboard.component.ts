45d6cb286c612a24916ce0bccecaff46c0991d7f
satish <satishkonduru0225@gmail.com>
2024-09-29 09:42:11 +0530

    Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    RouterModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, inject, OnInit, signal } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +13,41 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    MatMenuModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   preserveWhitespaces: true
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewChecked{
+
+  private _user = inject(UserService)
+  private _router = inject(Router)
+
+  public username = signal('')
+
+  ngOnInit(): void {
+    this.getUser()
+  }
+
+  ngAfterViewChecked(): void {
+    this.getUser()
+  }
+
+  getUser(){
+    const user = sessionStorage.getItem('userCredentials')
+    if(user){
+      const userDetails = this._user.retrieveCredentials().username
+      this.username.update(username => username = userDetails )
+    }
+  }
+
+
+  logout(){
+    sessionStorage.removeItem('userCredentials')
+    this.username.update(u => u='')
+    this._router.navigate(['/'])
+  }
 
 }
