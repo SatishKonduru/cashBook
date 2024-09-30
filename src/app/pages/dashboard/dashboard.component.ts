c6aee1dbc567312e9c8ba5b9bf3df9d94c9c0093
satish <satishkonduru0225@gmail.com>
2024-09-30 09:59:39 +0530

    Book List in Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -7,6 +7,8 @@ import {MatDividerModule} from '@angular/material/divider';
 import { RouterModule } from '@angular/router';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { NewBookComponent } from '../new-book/new-book.component';
+import { UserService } from '../../services/user.service';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,21 +19,56 @@ import { NewBookComponent } from '../new-book/new-book.component';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    RouterModule
+    RouterModule,
+    MatTooltipModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   
   public dialog = inject(MatDialog) 
+  user = inject(UserService)
+  books: any = []
   
+  ngOnInit(): void {
+    this.getBooks()
   
+  }
+
+
   addBook(){
     const dialogConfig = new MatDialogConfig()
     dialogConfig.autoFocus = true
     dialogConfig.disableClose = true
     dialogConfig.width = '700px'
-    this.dialog.open(NewBookComponent, dialogConfig)
+    const ref = this.dialog.open(NewBookComponent, dialogConfig)
+    ref.componentInstance.emitter.subscribe({
+      next: (res: any) => {
+        this.getBooks()
+      }
+    })
   }
+
+
+  getBooks(){
+    const {username, password} = this.user.retrieveCredentials()
+    this.user.getUsers().subscribe({
+      next: (res: any) => {
+        res.forEach(user => {
+          if(user.username == username && user.password == password){
+            this.books = user.books
+            console.log("Books:", this.books)
+          }
+        })
+      },
+      error: (err: any) => {
+        console.log("Error: ", err)
+      }
+    })
+
+
+  }
+
+
 }
