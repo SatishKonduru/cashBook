d5f93903a82e6911a3c6fc2daafd4b871490ad9b
satish <satishkonduru0225@gmail.com>
2024-10-02 11:52:50 +0530

    CashIn and CashOut Drawer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideToastr} from 'ngx-toastr';
 import { provideHttpClient } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideAnimationsAsync(),
     provideToastr(),
-    provideHttpClient()
+    provideHttpClient(),
+    provideNativeDateAdapter()
   
   ]
 };
--- src/app/pages/view-book/view-book.component.ts
+++ src/app/pages/view-book/view-book.component.ts
@@ -1,14 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatDividerModule} from '@angular/material/divider';
-
+import { ToastrService } from 'ngx-toastr';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatCalendar } from '@angular/material/datepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 @Component({
   selector: 'app-view-book',
   standalone: true,
@@ -20,35 +24,67 @@ import {MatDividerModule} from '@angular/material/divider';
               MatFormFieldModule,
               MatInputModule,
               FormsModule,
-              MatDividerModule
+              MatDividerModule,
+              MatToolbarModule,
+              ReactiveFormsModule,
+              MatCalendar,
+              MatDatepickerModule,
+              NgxMaterialTimepickerModule
             ],
   templateUrl: './view-book.component.html',
   styleUrl: './view-book.component.css',
-  preserveWhitespaces: true
+  preserveWhitespaces: true,
+  providers: [DatePipe]
 })
-export class ViewBookComponent {
+export class ViewBookComponent implements OnInit{
 activatedRoute = inject(ActivatedRoute)
 bookName: any = ''
 searchKey : any = ''
 router = inject(Router)
+isDrawerOpen = false
+addForm : any = FormGroup
+fb = inject(FormBuilder)
+userId : any
+toastr = inject(ToastrService)
+title : any
+entryCode = 0
+cashInMoney : number
+cashOutMoney : number
+datePipe = inject(DatePipe)
+
 constructor(){
   this.activatedRoute.queryParams.subscribe(p => this.bookName = p['book'])
   
 }
 
-
+ngOnInit(): void {
+  const currentTime = new Date().toLocaleTimeString('en-us', {hour: '2-digit', minute: '2-digit', hour12: true})
+  this.addForm = this.fb.group({
+    date: [new Date(), Validators.required],
+    time: [currentTime, Validators.required],
+    amount: [0, Validators.required],
+    description: ['', Validators.required]
+  })
+}
 goBack(){
 this.router.navigate(['/dashboard'])
 }
 
 
 toggleDrawerForCashIn(){
-
+  this.isDrawerOpen = !this.isDrawerOpen
+  this.title = 'Add Entry for CASH IN'
+  this.entryCode = 1
 }
 
 toggleDrawerForCashOut(){
-
+  this.isDrawerOpen = !this.isDrawerOpen
+  this.title = 'Add Entry for CASH OUT'
+  this.entryCode = 2
 }
 
+toggleDrawer(){
+  this.isDrawerOpen = !this.isDrawerOpen
+}
 
 }
